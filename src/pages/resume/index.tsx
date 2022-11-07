2a48e3fa32040aac2d9778bf0da6bb10cad8b455
CodeNate02 <n8opot8oW@gmail.com>
2022-11-06 22:24:59 -0500

    Skills section (version 1)

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,4 +1,4 @@
-
+import { useMemo } from "react";
 
 const Resume = () => {
   return (
@@ -43,7 +43,7 @@ const Resume = () => {
           </ul>
         </section>
         <br />
-        <section>
+        <section id='work'>
           <h3 className="w-auto text-2xl border-b-2 border-white font-raleway print:border-black">
             Work Experience:
           </h3>
@@ -52,11 +52,7 @@ const Resume = () => {
           ))}
         </section>
         <br />
-        <section>
-          <h4 className="w-auto text-2xl border-b-2 border-white print:border-black">
-            Skills:
-          </h4>
-        </section>
+        <SkillsSection />
       </div>
       <a className="sticky py-2 text-lg text-center underline text-blue-c hover:cursor-pointer hover:text-blue-b b-0 text-hidden bg-black/20">
         {/** NATE,
@@ -74,70 +70,100 @@ export default Resume;
 const WorkExp = ({ info }: any) => {
   return (
     <>
-    <div>
-      <b className="float-right"> {info.dates} </b>
-      <h5 className="font-bold"> {info.company} </h5>
-      <p className="float-right"> {info.location} </p>
-      <p> {info.title} </p>
-      <ul className="ml-5 list-disc">
-        {(info?.duties || []).map((item: string, index: number) => (
-          <li key={index}> {item} </li>
-        ))}
-      </ul>
-    </div>
-    <br/>
+      <div>
+        <b className="float-right"> {info.dates} </b>
+        <h5 className="font-bold"> {info.company} </h5>
+        <p className="float-right"> {info.location} </p>
+        <p> {info.title} </p>
+        <ul className="ml-5 list-disc">
+          {(info?.duties || []).map((item: string, index: number) => (
+            <li key={index}> {item} </li>
+          ))}
+        </ul>
+      </div>
+      <br />
     </>
-    
+  );
+};
+const SkillsSection = () => {
+  let skills = useMemo(
+    //@ts-ignore
+    () => Object.keys(SKILLS).filter((x) => SKILLS[x].length > 0),
+    []
+  );
+  console.log(skills);
+  return (
+    <section>
+      <h4 className="w-auto text-2xl border-b-2 border-white print:border-black">
+        Skills:
+      </h4>
+      {skills.map((item: string, index: number) => (
+        <ul className="grid grid-cols-3 mx-5 text-base" key={index}>
+          <h5 className="col-span-3 font-bold border-b border-white print:border-black">
+            {" "}
+            {item}:{" "}
+          </h5>
+          {
+            //@ts-ignore
+            SKILLS[item].map((item: string, index: number) => (
+              <li key={index}>{item}</li>
+            ))
+          }
+        </ul>
+      ))}
+    </section>
   );
 };
 
 const RELEVANT_COURSES = [
-    {
-      category: "Development",
-      courses: [
-        "Computer Problem Solving",
-        "Web & Mobile Development",
-        "Client Programming",
-        "Task Automation",
-        "Designing the User Experience ",
-      ],
-    },
-    {
-      category: "Administration",
-      courses: [
-        "Routing and Switching",
-        "Systems Administration",
-        "Config. Management",
-      ],
-    },
-    {
-      category: "Database",
-      courses: ["Database & Data Modeling", "Information Requirements Modeling"],
-    },
-  ];
-  const WORK_EXPERIENCE = [
-    {
-      company: "LSI Solutions",
-      dates: "August 2022 - Present",
-      location: "Victor, NY",
-      title: "IT Development Co-Op",
-      duties: [
-        "Improve company efficiency by developing applications for internal use.",
-      ],
-    },
-    {
-      company: "Burr and Burton Academy",
-      dates: "June 2018 - August 2019. Summers 2020 & 2021",
-      location: "Manchester, VT",
-      title: "Tech Department Internship",
-      duties: [
-        "Lessened load of senior IT department members by triaging incoming IT-related issues, questions, and requests.",
-        "Ensured prompt and reliable response to support requests through the use of Zendesk ticketing system.",
-        "Improved school live-stream events through the use of new hardware and software solutions.",
-        "Facilitated preparation, distribution, and collection of iPads and accessories used by the majority of the student body as a part of 1:1 iPad program.",
-      ],
-    },
-  ];
-  const SKILLS = [
-    'React'
-  ]
+  {
+    category: "Development",
+    courses: [
+      "Computer Problem Solving",
+      "Web & Mobile Development",
+      "Client Programming",
+      "Task Automation",
+      "Designing the User Experience ",
+    ],
+  },
+  {
+    category: "Administration",
+    courses: [
+      "Routing and Switching",
+      "Systems Administration",
+      "Config. Management",
+    ],
+  },
+  {
+    category: "Database",
+    courses: ["Database & Data Modeling", "Information Requirements Modeling"],
+  },
+];
+const WORK_EXPERIENCE = [
+  {
+    company: "LSI Solutions",
+    dates: "August 2022 - Present",
+    location: "Victor, NY",
+    title: "IT Development Co-Op",
+    duties: [
+      "Improve company efficiency by developing applications for internal use.",
+    ],
+  },
+  {
+    company: "Burr and Burton Academy",
+    dates: "June 2018 - August 2019. Summers 2020 & 2021",
+    location: "Manchester, VT",
+    title: "Tech Department Internship",
+    duties: [
+      "Lessened load of senior IT department members by triaging incoming IT-related issues, questions, and requests.",
+      "Ensured prompt and reliable response to support requests through the use of Zendesk ticketing system.",
+      "Improved school live-stream events through the use of new hardware and software solutions.",
+      "Facilitated preparation, distribution, and collection of iPads and accessories used by the majority of the student body as a part of 1:1 iPad program.",
+    ],
+  },
+];
+const SKILLS = {
+  Beginner: ["Angular", "C#", "Bash"],
+  Intermediate: ["Python", "Java", "MySQL", "jQuery"],
+  Proficient: ["HTML", "Javascript", "CSS", "React", "Typescript"],
+};
