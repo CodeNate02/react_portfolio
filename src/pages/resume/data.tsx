d30889ddc07c93fb882487c78d92f6b57f1a5f2c
CodeNate02 <n8opot8oW@gmail.com>
2022-12-14 22:42:37 -0500

    Makes updates to Resume data

--- src/pages/resume/data.tsx
+++ src/pages/resume/data.tsx
@@ -1,62 +1,59 @@
-
 export const RELEVANT_COURSES = [
-	{
-		category: 'Development',
-		courses: [
-			'Computer Problem Solving',
-			'Web & Mobile Development',
-			'Client Programming',
-			'Task Automation',
-			'Designing the User Experience ',
-		],
-	},
-	{
-		category: 'Administration',
-		courses: [
-			'Routing and Switching',
-			'Systems Administration',
-			'Config. Management',
-		],
-	},
-	{
-		category: 'Database',
-		courses: [
-			'Database & Data Modeling',
-			'Information Requirements Modeling',
-		],
-	},
+  {
+    category: "Development",
+    courses: [
+      "Computer Problem Solving",
+      "Web & Mobile Development",
+      "Client Programming",
+      "Task Automation",
+      "Designing the User Experience ",
+    ],
+  },
+  {
+    category: "Administration",
+    courses: [
+      "Routing and Switching",
+      "Systems Administration",
+      "Config. Management",
+    ],
+  },
+  {
+    category: "Database",
+    courses: ["Database & Data Modeling", "Information Requirements Modeling"],
+  },
 ];
 export const WORK_EXPERIENCE = [
-	{
-		company: 'LSI Solutions',
-		dates: 'August 2022 - Present',
-		location: 'Victor, NY',
-		title: 'IT Development Co-Op',
-		duties: [
-			'Improve company efficiency by developing applications for internal use.','Digitized New Hire & Position Change forms by devising form management web-app using React and NoSQL.',
-			'Recognized for timely completion of assigned projects.'
-		],
-	},
-	{
-		company: 'Burr and Burton Academy',
-		dates: 'June 2018 - August 2019. Summers 2020 & 2021',
-		location: 'Manchester, VT',
-		title: 'Tech Department Internship',
-		duties: [
-			'Lessened load of senior IT department members by triaging incoming IT-related issues, questions, and requests.',
-			'Ensured prompt and reliable response to support requests through the use of Zendesk ticketing system.',
-			'Improved school live-stream events through the use of new hardware and software solutions.',
-			'Facilitated preparation, distribution, and collection of iPads and accessories used by the majority of the student body as a part of 1:1 iPad program.',
-		],
-	},
+  {
+    company: "LSI Solutions",
+    dates: "August 2022 - December 2022",
+    location: "Victor, NY",
+    title: "IT Development Co-Op",
+    duties: [
+      "Improved company efficiency by developing applications for internal use.",
+      "Digitized New Hire & Position Change forms by devising form management web-app using React and NoSQL.",
+      "Recognized for timely completion of assigned projects.",
+    ],
+  },
+  {
+    company: "Burr and Burton Academy",
+    dates: "June 2018 - August 2019. Summers 2020 & 2021",
+    location: "Manchester, VT",
+    title: "Tech Department Internship",
+    duties: [
+      "Lessened load of senior IT department members by triaging incoming IT-related issues, questions, and requests.",
+      "Ensured prompt and reliable response to support requests through the use of Zendesk ticketing system.",
+      "Improved school live-stream events through the use of new hardware and software solutions.",
+      "Facilitated preparation, distribution, and collection of iPads and accessories used by the majority of the student body as a part of 1:1 iPad program.",
+    ],
+  },
 ];
 export const SKILLS = {
-	Beginner: ['PHP', 'Angular', 'C#', 'Bash', 'Electron'],
-	Intermediate: ['Python', 'Java', 'MySQL', 'jQuery'],
-	Proficient: ['HTML', 'Javascript', 'CSS', 'React', 'Typescript'],
-	OS: [
-		'Windows XP, Vista, 7-11: Advanced',
-		'Linux (RHEL, Ubuntu, CentOS 7-8): Beginner',
-		'MacOS: Intermediate',
-	],
+  Beginner: ["PHP", "Angular", "C#", "Bash", "Electron"],
+  Intermediate: ["Python", "Java", "MySQL", "jQuery","Node.js"],
+  Proficient: ["HTML", "Javascript", "CSS", "React", "Typescript"],
+  OS: [
+    "Windows XP, Vista, 7-11: Advanced",
+    "Linux (RHEL, Ubuntu, CentOS 7-8): Beginner",
+    "MacOS: Intermediate",
+  ],
 };
